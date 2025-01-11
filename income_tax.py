ef15f7711b796e6e9d78fbe3e309e9d40aba34de
Sneha Rose Vinod <sneharosevinod2026@gmail.com>
2025-01-12 00:42:43 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,2 +1,17 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+#income tax calculation
+
+income=float(input("Enter your annual income"))
+tax=0
+if(income<=250000):
+    tax=0
+elif (income<=500000):
+    tax=(income-250000)*0.05
+elif (income<=1000000):
+    tax=250000*0.05   
+    tax=tax+(income-500000)*0.2
+elif (income>1000000):
+    tax=250000*0.05
+    tax=tax+500000*0.2
+    tax=tax+(income-1000000)*0.3
+
+print("Tax=",tax)

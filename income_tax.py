aef768b92cf06b1f998d723016eaec72b86b1e27
Sneha-rose <sneharosevinod2026@gmail.com>
2025-01-20 22:10:49 +0530

    income_tax.py

--- income_tax.py
+++ income_tax.py
@@ -2,16 +2,16 @@
 
 income=float(input("Enter your annual income"))
 tax=0
-if(income<=250000):
+if(income<=300000):
     tax=0
-elif (income<=500000):
-    tax=(income-250000)*0.05
+elif (income<=700000):
+    tax=(income-300000)*0.05
 elif (income<=1000000):
-    tax=250000*0.05   
-    tax=tax+(income-500000)*0.2
-elif (income>1000000):
-    tax=250000*0.05
-    tax=tax+500000*0.2
-    tax=tax+(income-1000000)*0.3
+    tax=(700000 - 300000)*0.05 
+    tax += (income-700000)*0.2
+else:
+    tax = (700000 - 300000) *0.05
+    tax += (1000000 - 700000)*0.2
+    tax += (income-1000000)*0.3
 
 print("Tax=",tax)

9a5b4f9b47ec75f23a0fd26368a66abe50bfd6ad
Sneha-rose <sneharosevinod2026@gmail.com>
2025-01-29 21:20:29 +0530

    flesch_index.py

--- flesch_index/flesch_index.py
+++ flesch_index/flesch_index.py
@@ -13,11 +13,15 @@ score=206.835-(1.015*(total_words/total_sentence))-(84.6*(total_syllable/total_w
 grade=(0.39*(total_words/total_sentence))+(11.8*(total_syllable/total_words))-15.59
 print("Score: ",score)
 print("Grade: ",grade)
-if score>=0 and score<30:
+if score >= 0 and score < 30:
     print("COLLEGE LEVEL")
-        
-elif score>=50 and score<60:
+elif score >= 30 and score < 50:
+    print("HIGHER SECONDARY")
+elif score >= 50 and score < 60:
     print("HIGH SCHOOL")
-
-elif score>=90 and score<=100:
-    print("FORTH GRADE")
+elif score >= 60 and score < 90:
+    print("FIFTH GRADE")
+elif score >= 90 and score <= 100:
+    print("FOURTH GRADE")
+else:
+    print("INVALID SCORE")
